cf3f0f4017cd04162efadf72a931ba9043a3634a
Nikiforos <nikifmak@gmail.com>
2017-10-24 14:52:39 +0300

    Update encode.py
    Removed useless function

--- encode.py
+++ encode.py
@@ -1,17 +1,6 @@
 import os
 import base64
 
-
-def convert42():
-    with open("sample.xml", "rb") as imageFile:
-        str = base64.b64encode(imageFile.read())
-        str = bytearray(str,"utf-8")
-        outputFile = open('sample.bin', 'wb+')
-        outputFile.write(str)
-        outputFile.close()
-
-
-
 def convertToBase64(filename):
     f = open(filename, 'rb')
     data = f.read()
@@ -24,9 +13,7 @@ def convertToBase64(filename):
     outputFile = open(outputFilePath, 'wb+')
     outputFile.write(bytearray(string))
     outputFile.close()
-
-
-
+    
 if __name__ == "__main__":
     for filename in os.listdir(os.getcwd()):
         convertToBase64(filename)
